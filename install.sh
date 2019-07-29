2a1dcae88bd7bd7df0470103c4825dc2c30ab526
Cave Escape Pi <jamesfowkes@gmail.com>
2019-07-30 00:53:41 +0100

    Update installer with run.py

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ pip install -r requirements.txt
 
 path_param=$1
 
-chmod a+x tft_projector.py
+chmod a+x run.py
 thisdir=`pwd`
 cat tftprojector.service.template \
 | sed -e "s#EXEC_START_PATH#$thisdir/tft_projector.py#" \
